fc2cda946935c8fffb4abe6c4f7defc4e6c888a0
deep12121994 <balasdeep@gmail.com>
2022-01-07 23:38:13 +0530

    componet file created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ListPage from "./component/ListPage";
+import DetailPage from "./component/DetailPage";
+
+const App = () => {
+  return (
+    <>
+      <ListPage />
+      <DetailPage />
+    </>
+  );
+};
+
+export default App;
